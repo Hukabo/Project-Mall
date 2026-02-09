6c036ae15280209363986be6aed6ff18260fb5c8
Hukabo <dlwlsdn153@gmail.com>
2026-02-09 20:28:46 +0900

    feat: bind filter and logger

--- server/nest_server/src/app.module.ts
+++ server/nest_server/src/app.module.ts
@@ -1,9 +1,37 @@
-import { Module } from '@nestjs/common';
-import { UserController } from './user/controllers/user.controller';
-import { UserService } from './user/services/user.service';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
+import { UserModule } from './user/modules/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { User } from './user/entity/user.entity';
+import { logger } from './middlewares/logger.middleware';
 
 @Module({
-  controllers: [UserController],
-  providers: [UserService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '123123',
+      database: 'test',
+      entities: [User],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  constructor(private dataSource: DataSource) {}
+
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(logger).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
--- server/nest_server/src/main.ts
+++ server/nest_server/src/main.ts
@@ -1,8 +1,12 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CatchEverythingFilter } from './filters/all-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const httpAdapterHost = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new CatchEverythingFilter(httpAdapterHost));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
