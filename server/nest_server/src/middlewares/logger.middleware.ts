0ad993049632752af5fcd962f6652147a362cabd
Hukabo <dlwlsdn153@gmail.com>
2026-02-09 20:26:19 +0900

    feat: logger

---
+++ server/nest_server/src/middlewares/logger.middleware.ts
@@ -0,0 +1,9 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+
+export function logger(req: Request, res: Response, next: NextFunction) {
+  console.log(
+    `method: ${req.method}, from: ${req.ip}, to: ${req.originalUrl}, at: ${new Date().toISOString()}`,
+  );
+  next();
+}
