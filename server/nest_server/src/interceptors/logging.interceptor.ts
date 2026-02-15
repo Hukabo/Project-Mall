99c35cbab9310d97ca8983d179edbded25b54c8b
Hukabo <dlwlsdn153@gmail.com>
2026-02-15 19:15:08 +0900

    feat: logging & transform interceptor

---
+++ server/nest_server/src/interceptors/logging.interceptor.ts
@@ -0,0 +1,27 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Request } from 'express';
+
+import { Observable, tap } from 'rxjs';
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    const req = context.switchToHttp().getRequest<Request>();
+
+    console.log('path: ', req.url);
+    console.log('Before...');
+
+    const now = Date.now();
+    return next
+      .handle()
+      .pipe(tap(() => console.log(`After... ${Date.now() - now}ms`)));
+  }
+}
--- server/nest_server/src/main.ts
+++ server/nest_server/src/main.ts
@@ -1,11 +1,14 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CatchEverythingFilter } from './filters/all-exception.filter';
+import { LoggingInterceptor } from './interceptors/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const httpAdapterHost = app.get(HttpAdapterHost);
+
   app.useGlobalFilters(new CatchEverythingFilter(httpAdapterHost));
+  app.useGlobalInterceptors(new LoggingInterceptor());
 
   await app.listen(process.env.PORT ?? 3000);
 }
