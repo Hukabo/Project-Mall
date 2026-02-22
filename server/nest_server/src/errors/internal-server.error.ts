d707986e0cfbc013b59c9426fc2b0e0274d277b5
hukabo <dlwlsdn153@gmail.com>
2026-02-22 22:12:12 +0900

    fix: correct status code

--- server/nest_server/src/errors/internal-server.error.ts
+++ server/nest_server/src/errors/internal-server.error.ts
@@ -4,7 +4,7 @@ export class InternalServerError extends HttpException {
   constructor(message: string, error?: any) {
     super(
       {
-        status: HttpStatus.NOT_ACCEPTABLE,
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
         message,
         error: 'Internal Server Error',
       },
