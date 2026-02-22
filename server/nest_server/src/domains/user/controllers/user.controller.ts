7e96e6496c976a3473c78e409a62b97c5a03eb95
hukabo <dlwlsdn153@gmail.com>
2026-02-22 22:01:32 +0900

    fix: correct import paths and update user modification method to use proper DTO validation

--- server/nest_server/src/domains/user/controllers/user.controller.ts
+++ server/nest_server/src/domains/user/controllers/user.controller.ts
@@ -15,10 +15,7 @@ import {
   UseGuards,
   UsePipes,
 } from '@nestjs/common';
-import {
-  type CreateUserDto,
-  createUserSchema,
-} from '../dto/create-user.schema';
+import { type CreateUserDto, createUserSchema } from '../dto/create-user.dto';
 import { UserService } from '../services/user.service';
 import { User } from '../entity/user.entity';
 import { ValidationPipe } from 'src/pipes/validation.pipe';
@@ -26,6 +23,7 @@ import { RoleGuard } from 'src/guards/roles.guard';
 import { Roles } from 'src/decorators/roles.decorator';
 import { Role } from 'src/enums/role.enum';
 import { DeleteResult } from 'typeorm';
+import { type UpdateUserDto, updateUserSchema } from '../dto/update-user.dto';
 
 @Controller('users')
 @UseGuards(RoleGuard)
@@ -37,7 +35,6 @@ export class UserController {
   }
 
   @Post()
-  @Roles([Role.ADMIN, Role.USER])
   @UsePipes(new ValidationPipe(createUserSchema))
   async create(@Body() createUserDto: CreateUserDto): Promise<User> {
     const res = await this.userService.create(createUserDto);
@@ -61,8 +58,13 @@ export class UserController {
   }
 
   @Patch(':id')
-  modify(@Param('id', ParseIntPipe) id: number): unknown {
-    return;
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body(new ValidationPipe(updateUserSchema)) updateUserDto: UpdateUserDto,
+  ): Promise<User> {
+    const res = this.userService.update(id, updateUserDto);
+
+    return res;
   }
 
   @Delete(':id')
