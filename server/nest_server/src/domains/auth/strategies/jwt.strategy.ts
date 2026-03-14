f1a098af29ca4580085edbe6419c36c65423d574
Hukabo <dlwlsdn153@gmail.com>
2026-03-14 21:05:55 +0900

    feat: enhance JwtStrategy to validate user

--- server/nest_server/src/domains/auth/strategies/jwt.strategy.ts
+++ server/nest_server/src/domains/auth/strategies/jwt.strategy.ts
@@ -4,12 +4,14 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import jwtConfig from 'src/config/jwt.config';
 import { AuthJwtPayload } from 'src/domains/auth/types/auth-jwtPayload';
+import { AuthService } from '../services/auth.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
     @Inject(jwtConfig.KEY)
     private jwtConfiguration: ConfigType<typeof jwtConfig>,
+    private authService: AuthService,
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -17,9 +19,11 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  validate(payload: AuthJwtPayload) {
-    console.log(payload);
+  async validate(payload: AuthJwtPayload) {
+    const userId = payload.sub;
 
-    return { id: payload.sub };
+    const currentUser = await this.authService.validateJwtUser(userId);
+
+    return currentUser;
   }
 }
