6756b67a3e433398070b2731c687be5851296eeb
hukabo <dlwlsdn153@gmail.com>
2026-07-26 13:45:22 +0900

    chore: remove unused imports from Home component for cleaner code

--- client/mall/app/page.tsx
+++ client/mall/app/page.tsx
@@ -1,13 +1,4 @@
 import Header from "./_component/Header";
-import Sidebar from "./_component/Sidebar";
-import { useContext, useEffect, useState } from "react";
-import { getUser } from "./_lib/api/user";
-import { UserContext } from "./_lib/context/UserProvider";
-import { User } from "./_lib/types/user";
-import { Product } from "./_lib/types/product";
-import ProductCard from "./_component/ProductCard";
-import Link from "next/link";
-import ProductList from "./_component/ProductList";
 import Body from "./_component/Body";
 
 export default function Home() {
