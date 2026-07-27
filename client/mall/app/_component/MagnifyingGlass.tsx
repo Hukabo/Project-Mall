5abfc75337aa52b1660380f802f978c6e2a9e016
hukabo <dlwlsdn153@gmail.com>
2026-07-27 16:03:36 +0900

    feat: MagnifyingGlass 컴포넌트 추가

---
+++ client/mall/app/_component/MagnifyingGlass.tsx
@@ -0,0 +1,14 @@
+export default function MagnifyingGlass({ className }: { className?: string }) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      className={className}
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+    >
+      <circle cx="11" cy="11" r="7" />
+      <line x1="21" y1="21" x2="16.65" y2="16.65" />
+    </svg>
+  );
+}
