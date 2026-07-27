3a1c1f7543987b452ea792f4a9b9a5c736f601d3
hukabo <dlwlsdn153@gmail.com>
2026-07-27 16:05:19 +0900

    feat: 회원가입 페이지에 주소 입력 폼 및 API 연동 추가

--- client/mall/app/join/page.tsx
+++ client/mall/app/join/page.tsx
@@ -3,15 +3,24 @@ import { error } from "console";
 import { redirect } from "next/dist/server/api-utils";
 import Image from "next/image";
 import { SyntheticEvent, useState } from "react";
+import { api } from "../_lib/api/api";
+import { User } from "../_lib/types/user";
+import ShippingAddressForm from "../_component/ShippingAdressForm";
 
 export default function JoinPage() {
+  const [address, setAddress] = useState<AddressForm>({
+    zonecode: "",
+    roadAddress: "",
+    detailAddress: "",
+  });
+
   const [form, setForm] = useState({
     email: "",
     username: "",
     password: "",
     confirmPassword: "",
     birth: "",
-    address: "",
+    address,
     phone: "",
   });
 
@@ -23,32 +32,30 @@ export default function JoinPage() {
       return;
     }
 
-    const res = await fetch("http://localhost:8080/users", {
-      method: "POST",
-      headers: { "Content-type": "application/json" },
-      body: JSON.stringify(form),
-    });
+    const res = await api.post<{
+      id: number;
+      username: string;
+      roles: string[];
+    }>("users", form);
 
-    const data = await res.json();
-
-    console.log("data = ", data);
-    alert(`${data.username} 회원가입 완료`);
+    alert(`${res.username} 회원가입 완료`);
   }
 
   return (
     <div className="min-h-screen flex justify-center items-center">
-      <div className="flex-1 p-8 max-w-1/2 border border-grey-light-1 rounded-md shadow-dark">
+      <div className="flex-1 p-8 max-w-1/2 border border-grey-light-1 rounded-md shadow-dark bg-surface">
         <div className="flex justify-between items-center mb-3">
           <h1 className="flex-1 text-3xl text-grey-dark-1 font-thin">
             회원가입
           </h1>
           <a href="/" className="inline-block">
-            <Image
+            {/* <Image
               src="/svg/home.svg"
               alt="Home image"
               width={40}
               height={40}
-            />
+            /> */}
+            <h1 className="text-2xl italic uppercase">Mall</h1>
           </a>
         </div>
         <form
@@ -61,7 +68,7 @@ export default function JoinPage() {
             id="email"
             name="email"
             placeholder="이메일"
-            className="p-2 bg-grey-light-1 rounded-md focus:outline-none"
+            className="p-2 bg-grey-light-1 rounded-md focus:outline-none border border-line"
             onChange={(e) => {
               setForm({ ...form, email: e.target.value });
             }}
@@ -73,7 +80,7 @@ export default function JoinPage() {
             id="name"
             name="name"
             placeholder="이름"
-            className="p-2 bg-grey-light-1 rounded-md focus:outline-none"
+            className="p-2 bg-grey-light-1 rounded-md focus:outline-none border border-line"
             onChange={(e) => {
               setForm({ ...form, username: e.target.value });
             }}
@@ -85,7 +92,7 @@ export default function JoinPage() {
             id="birth"
             name="birth"
             placeholder="생년월일"
-            className="p-2 bg-grey-light-1 rounded-md focus:outline-none"
+            className="p-2 bg-grey-light-1 rounded-md focus:outline-none border border-line"
             onChange={(e) => {
               setForm({ ...form, birth: e.target.value });
             }}
@@ -97,7 +104,7 @@ export default function JoinPage() {
             id="password"
             name="password"
             placeholder="비밀번호"
-            className="p-2 bg-grey-light-1 rounded-md focus:outline-none"
+            className="p-2 bg-grey-light-1 rounded-md focus:outline-none border border-line"
             onChange={(e) => {
               setForm({ ...form, password: e.target.value });
             }}
@@ -109,23 +116,13 @@ export default function JoinPage() {
             id="confirm-pass"
             name="confirm-pass"
             placeholder="비밀번호 확인"
-            className="p-2 bg-grey-light-1 rounded-md focus:outline-none"
+            className="p-2 bg-grey-light-1 rounded-md focus:outline-none border border-line"
             onChange={(e) => {
               setForm({ ...form, confirmPassword: e.target.value });
             }}
           />
 
-          <label htmlFor="address">주소지: {""}</label>
-          <input
-            type="text"
-            id="address"
-            name="address"
-            placeholder="주소지"
-            className="p-2 bg-grey-light-1 rounded-md focus:outline-none"
-            onChange={(e) => {
-              setForm({ ...form, address: e.target.value });
-            }}
-          />
+          <ShippingAddressForm address={address} setAddress={setAddress} />
 
           <label htmlFor="phone">연락처: {""}</label>
           <input
@@ -133,7 +130,7 @@ export default function JoinPage() {
             id="phone"
             name="phone"
             placeholder="연락처"
-            className="p-2 bg-grey-light-1 rounded-md focus:outline-none"
+            className="p-2 bg-grey-light-1 rounded-md focus:outline-none border border-line"
             onChange={(e) => {
               setForm({ ...form, phone: e.target.value });
             }}
@@ -141,7 +138,7 @@ export default function JoinPage() {
 
           <button
             type="submit"
-            className="bg-pink-200 py-2 px-8 rounded-md cursor-pointer mt-4"
+            className="bg-ink text-surface py-2 px-8 rounded-md cursor-pointer mt-4"
           >
             제출하기
           </button>
