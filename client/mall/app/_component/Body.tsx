b98c9cc6deee64da39806d9cb13bf83b59c10b5f
hukabo <dlwlsdn153@gmail.com>
2026-08-16 16:30:19 +0900

    feat: Body 컴포넌트에 반응형 사이드바 추가 및 레이아웃 수정

--- client/mall/app/_component/Body.tsx
+++ client/mall/app/_component/Body.tsx
@@ -1,10 +1,13 @@
 import ProductList from "./ProductList";
+import ResponsiveSidebar from "./ResponsiveSidebar";
 import Sidebar from "./Sidebar";
 
 export default function Body() {
   return (
-    <main className="flex">
-      <Sidebar />
+    <main className="flex-1 flex">
+      <ResponsiveSidebar>
+        <Sidebar />
+      </ResponsiveSidebar>
       <ProductList />
     </main>
   );
