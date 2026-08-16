f53468411b59d5f20036fbd768bee70a006b3dae
hukabo <dlwlsdn153@gmail.com>
2026-08-16 16:40:47 +0900

    feat: ConfigModule을 CloudinaryModule에 추가

--- server/nest_server/src/domains/image/module/cloudinary.module.ts
+++ server/nest_server/src/domains/image/module/cloudinary.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CloudinaryProvider } from '../../../config/cloudinary.config';
 import { CloudinaryService } from '../service/cloudinary.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [ConfigModule],
   providers: [CloudinaryProvider, CloudinaryService],
   exports: [CloudinaryService],
 })
