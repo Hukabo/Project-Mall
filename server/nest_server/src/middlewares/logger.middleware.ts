45bc31f6ecfc8ceb9bf4424d2e8aba8f90cfcaa8
hukabo <dlwlsdn153@gmail.com>
2026-08-16 16:45:08 +0900

    fix: 로그 미들웨어의 타임스탬프 형식을 한국어 로케일로 변경

--- server/nest_server/src/middlewares/logger.middleware.ts
+++ server/nest_server/src/middlewares/logger.middleware.ts
@@ -4,7 +4,12 @@ import { NextFunction, Request, Response } from 'express';
 export function logger(req: Request, res: Response, next: NextFunction) {
   console.log('middle ware: ');
   console.log(
-    `method: ${req.method}, from: ${req.ip}, to: ${req.originalUrl}, at: ${new Date().toISOString()}\n`,
+    `method: ${req.method}, from: ${req.ip}, to: ${req.originalUrl}, at: ${new Date().toLocaleString(
+      'ko-KR',
+      {
+        timeZone: 'Asia/Seoul',
+      },
+    )}\n`,
   );
   next();
 }
