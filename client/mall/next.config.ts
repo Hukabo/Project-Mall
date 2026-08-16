2ed1b422fe6e14c9765e26cbdc64a9bbef1f303a
hukabo <dlwlsdn153@gmail.com>
2026-08-16 16:27:18 +0900

    feat: 이미지 설정에서 로컬 프로토콜 주석 처리 및 Cloudinary로 변경

--- client/mall/next.config.ts
+++ client/mall/next.config.ts
@@ -10,9 +10,13 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "8080",
+        // 로컬 이미지
+        // protocol: "http",
+        // hostname: "localhost",
+        // port: "8080",
+
+        protocol: "https",
+        hostname: "res.cloudinary.com",
       },
     ],
   },
