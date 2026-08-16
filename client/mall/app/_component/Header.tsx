91dbfb2b33b10327dcbdfbd9ac193b9f84fe203b
hukabo <dlwlsdn153@gmail.com>
2026-08-16 16:31:06 +0900

    feat: Header 컴포넌트에서 모바일 사이드바 버튼 추가 및 코드 정리

--- client/mall/app/_component/Header.tsx
+++ client/mall/app/_component/Header.tsx
@@ -1,28 +1,41 @@
 "use client";
 
-import Image from "next/image";
 import { useContext } from "react";
-import { UserContext } from "../_lib/context/UserProvider";
+import { UserContext } from "../_lib/provider/UserProvider";
 import Link from "next/link";
-import { logout } from "../_lib/api/user";
-import { useRouter } from "next/navigation";
-import { User } from "../_lib/types/user";
+import Image from "next/image";
 import SearchBar from "./SearchBar";
+import { useMobileSidebar } from "./MobileSidebarProvider";
 
 export default function Header() {
   const { user, logout } = useContext(UserContext);
+  const { open } = useMobileSidebar();
 
   return (
     <header className="py-3 px-12 flex justify-between shadow-2xs bg-surface border-b border-grey-light-4">
-      <Link href="/" className="inline-block">
-        <h1 className="text-2xl italic uppercase">Mall</h1>
-      </Link>
+      <div className="flex items-center gap-3">
+        <button
+          type="button"
+          onClick={open}
+          aria-label="카테고리 메뉴 열기"
+          className="rounded-md p-2 -ml-4 transition-colors hover:bg-grey-light-2 min-[1260px]:hidden"
+        >
+          <Image
+            src="/svg/hamburger.svg"
+            alt="menu button"
+            width={24}
+            height={24}
+            sizes="(max-width: 1200px) 5vw"
+          />
+        </button>
+        <Link href="/" className="inline-block">
+          <h1 className="text-2xl italic uppercase">Mall</h1>
+        </Link>
+      </div>
       <SearchBar />
       <div className="flex items-center gap-3 font-medium">
         {user ? (
           <>
-            <span className="border-e pr-3">{user.username}님</span>
-
             <Link href={`/user`} className="border-e pr-3 hover:text-moss">
               내정보
             </Link>
