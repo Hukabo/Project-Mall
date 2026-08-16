0fcc60e15cc0e8f2d0bbe44ce168dc506c0c35f8
hukabo <dlwlsdn153@gmail.com>
2026-08-16 16:44:47 +0900

    fix: PaymentMethod 열거형 삭제

--- server/nest_server/src/enums/payment.enum.ts
+++
@@ -1,8 +0,0 @@
-// type PaymentMethod = "card" | "transfer" | "kakaopay";
-
-export enum PaymentMethod {
-  CREDIT_CARD = 'card',
-  TRANSFER = 'transfer',
-  TOSS = 'toss',
-  KAKAOPAY = 'kakaopay',
-}
