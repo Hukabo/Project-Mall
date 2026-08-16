6338c4195203960caf17f3af5fe608f95598131b
hukabo <dlwlsdn153@gmail.com>
2026-08-16 16:33:12 +0900

    fix: Sidebar 컴포넌트에서 ListItem을 Categories로 변경 및 스타일 수정

--- client/mall/app/_component/Sidebar.tsx
+++ client/mall/app/_component/Sidebar.tsx
@@ -1,16 +1,16 @@
 import { api } from "../_lib/api/api";
-import { Category } from "../_lib/types/category";
-import ListItem from "./ListItem";
+import { Category } from "../_lib/types/category/category";
+import Categories from "./Categories";
 
 export default async function Sidebar() {
   const categories = await api.get<Category[]>("category/parent");
 
   return (
-    <section className="h-[120rem] w-[12vw] p-7 bg-surface">
+    <section className="h-full w-full overflow-y-auto bg-surface p-7 min-[1260px]:h-[120rem] min-[1260px]:w-[12vw]">
       <nav>
         <ul className="flex flex-col gap-6 mt-5">
           {categories.map((category) => (
-            <ListItem key={category.id} category={category} />
+            <Categories key={category.id} category={category} />
           ))}
         </ul>
       </nav>
