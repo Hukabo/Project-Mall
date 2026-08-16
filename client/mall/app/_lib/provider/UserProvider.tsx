155dd9d37f37ab5d780e9cb13ab2fdc2fd5e0368
hukabo <dlwlsdn153@gmail.com>
2026-08-16 16:36:10 +0900

    feat: UserProvider 컴포넌트 추가 및 사용자 프로필 관리 기능 구현

---
+++ client/mall/app/_lib/provider/UserProvider.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { createContext, useEffect, useState } from "react";
+import { User } from "../types/user/user";
+import Loading from "@/app/_component/Loading";
+import { api } from "../api/api";
+
+interface UserContextType {
+  user: User | null;
+  loading: boolean;
+  refetchUser: () => Promise<void>;
+  logout: () => Promise<void>;
+}
+
+export const UserContext = createContext<UserContextType>({
+  user: null,
+  loading: true,
+  refetchUser: async () => {},
+  logout: async () => {},
+});
+
+export default function UserProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  async function refetchUser() {
+    try {
+      const user = await api.get<User>("users/profile");
+
+      setUser(user);
+    } catch (error) {
+      console.error(error);
+      setUser(null);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function logout() {
+    await api.patch("auth/logout");
+    setUser(null);
+  }
+
+  useEffect(() => {
+    refetchUser();
+  }, []);
+
+  if (loading) return <Loading />;
+
+  return (
+    <UserContext value={{ user, loading, refetchUser, logout }}>
+      {children}
+    </UserContext>
+  );
+}
