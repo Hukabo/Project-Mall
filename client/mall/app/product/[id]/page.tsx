953cffc199f2da9d0eeb8c2ca69c2406c30db314
hukabo <dlwlsdn153@gmail.com>
2026-08-16 16:37:40 +0900

    fix: Props 타입 경로 수정 및 불필요한 임포트 제거

--- client/mall/app/product/[id]/page.tsx
+++ client/mall/app/product/[id]/page.tsx
@@ -1,13 +1,8 @@
 import { notFound } from "next/navigation";
-import { Props } from "../../_lib/types/props";
-
+import { Props } from "../../_lib/types/props/props";
 import { Product } from "../../_lib/types/product/product";
-import Link from "next/link";
-import ImageGallery from "../../_component/ImageGallery";
-import ProductActions from "../../_component/QuantityBtn";
 import { api } from "@/app/_lib/api/api";
-import { useState } from "react";
-import ProductClient from "@/app/_component/product/ProductClient";
+import ProductClient from "@/app/_component/ProductClient";
 
 export default async function ProductPage({ params }: Props) {
   const id = Number((await params).id);
