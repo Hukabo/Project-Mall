a96b73a674f7d9df5b27d7a8fca42037d773936f
hukabo <dlwlsdn153@gmail.com>
2026-08-20 02:49:43 +0900

    fix: Public 데코레이터를 TestController에 추가

--- server/nest_server/src/domains/test/test.controller.ts
+++ server/nest_server/src/domains/test/test.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, Response } from '@nestjs/common';
+import { Public } from 'src/decorators/public.decorator';
 
 @Controller('test')
 export class TestController {
+  @Public()
   @Get()
   hello(): string {
     return 'hello';
