c639c5e9bedf500a3a04449a5abef84c4afca200
hukabo <dlwlsdn153@gmail.com>
2026-08-22 18:33:29 +0900

    refactor: 데이터베이스 엔티티 임포트 방식 변경 및 동기화 설정 수정

--- server/nest_server/src/config/db.config.ts
+++ server/nest_server/src/config/db.config.ts
@@ -1,18 +1,6 @@
 import { registerAs } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { Cart } from 'src/domains/cart/entity/cart.entity';
-import { CartItem } from 'src/domains/cart/cart_item/entity/cartItem.entity';
-import { Category } from 'src/domains/category/entity/category.entity';
-import { Product } from 'src/domains/product/entity/product.entity';
-import { User } from 'src/domains/user/entity/user.entity';
-import { Order } from 'src/domains/order/entity/order.entity';
-import { OrderItem } from 'src/domains/order/orderItem/entity/orderItem.entity';
-import { Payment } from 'src/domains/payment/entity/payment.entity';
-import { Shipping } from 'src/domains/order/shipping/shipping.entity';
-import { Image } from 'src/domains/image/entity/image.entity';
-import { ProductView } from 'src/domains/product/entity/productView.entity';
-import { ProductSpec } from 'src/domains/product/entity/productSpec.entity';
-import { Address } from 'src/domains/user/address/entity/address.entity';
+import { join } from 'path';
 
 export default registerAs('database', (): TypeOrmModuleOptions => ({
   type: 'postgres',
@@ -21,22 +9,9 @@ export default registerAs('database', (): TypeOrmModuleOptions => ({
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  entities: [
-    User,
-    Product,
-    ProductView,
-    ProductSpec,
-    Image,
-    Category,
-    Cart,
-    CartItem,
-    Order,
-    OrderItem,
-    Payment,
-    Shipping,
-    Address,
-  ],
-  synchronize: process.env.NODE_ENV !== 'production',
+  entities: [join(__dirname, '..', '/domains/**/entity/*.entity{.js,.ts}')],
+  synchronize: false,
+
   /* 개발 환경에서는 ssl 주석 처리 */
   // ssl: {
   //   rejectUnauthorized: false,
