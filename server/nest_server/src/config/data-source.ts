fc1bb57f56e33f2fc16ff35fd89a85583422c9cc
hukabo <dlwlsdn153@gmail.com>
2026-08-23 16:12:29 +0900

    feat: 데이터 소스 설정 파일 추가

---
+++ server/nest_server/src/config/data-source.ts
@@ -0,0 +1,14 @@
+import { DataSource } from 'typeorm';
+import { join } from 'path';
+
+const AppdataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT!, 10) || 5432,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  entities: [join(__dirname, '..', '/domains/**/entity/*.entity{.js,.ts}')],
+  synchronize: false,
+  migrations: [join(__dirname, '..', '/migrations/**/*{.js,.ts}')],
+});
