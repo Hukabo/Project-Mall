a78daf600b1906111642c8dfb573713ec8fa4d30
hukabo <dlwlsdn153@gmail.com>
2026-09-02 16:18:34 +0900

    feat: nest-api를 위한 ecosystem.config.js 파일 추가

---
+++ server/nest_server/ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: 'nest-api',
+      script: 'dist/main.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      env: {
+        NODE_ENV: 'production',
+        UV_THREADPOOL_SIZE: 64,
+      },
+    },
+  ],
+};
